6358b2e8c4a36ff6c9c3aa05b09c07e39d895436
Felix Arntz <felix-arntz@t-online.de>
2016-11-10 18:54:56 +0100

    fixed critical bug in GraphicalUIAlternativeTextsImages

--- includes/ScreenReaderCheck/Tests/GraphicalUIAlternativeTextsImageMaps.php
+++ includes/ScreenReaderCheck/Tests/GraphicalUIAlternativeTextsImageMaps.php
@@ -68,7 +68,7 @@ class GraphicalUIAlternativeTextsImageMaps extends Test {
 
 			if ( ! $alt ) {
 				$result['message_codes'][] = 'missing_alternative_text';
-				$result['messages'][] = $this->wrap_message( __( 'The following <code>area</code> tag of an image map is missing an alternative text:', 'screen-reader-check' ) . '<br>' . $this->wrap_code( $image->outerHtml() ), $image->getLineNo() );
+				$result['messages'][] = $this->wrap_message( __( 'The following <code>area</code> tag of an image map is missing an alternative text:', 'screen-reader-check' ) . '<br>' . $this->wrap_code( $area->outerHtml() ), $area->getLineNo() );
 				$has_errors = true;
 			}
 		}
